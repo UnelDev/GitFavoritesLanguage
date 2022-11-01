b22575d759120ce6360ea43fee7c44d1a98993c6
unel <enee.prog@gmail.com>
2022-11-01 21:22:46 +0100

    list all comit of user BOOM

--- index.ts
+++ index.ts
@@ -13,21 +13,14 @@ async function listLanguage(user: string, repo: string) {
     console.log(res.data)
 }
 async function getListRepo(user: string) {
-    console.log('list of langage of ' + user);
     const res = await axios.get('https://api.github.com/users/' + user + '/repos');
-    res.data.forEach(element => {
-        listLanguage(user, element.name)
-    });
+    return res.data;
 }
 async function getListOrgs(user: string) {
-    console.log('list of orgs of ' + user);
     const res = await axios.get('https://api.github.com/users/' + user + '/orgs');
-    res.data.forEach(element => {
-        console.log(element.login);
-    });
+    return res.data;
 }
 async function getListRepoOfOrg(org: string) {
-    console.log('list of Repo of ' + org);
     const res = await axios.get('https://api.github.com/orgs/' + org + '/repos');
     return res.data;
 }
@@ -38,7 +31,7 @@ async function mixMap(listMap: Array<Promise<Map<string, { additions: number, de
     // await Promise.all -> for create a async forEach
     const test = listMap.map(async Element => {
         // calback of map
-        return new Promise(async (resolve, reject) => {
+        return new Promise(async (resolve) => {
             // Element is a promise<map> create forEach of map 
             (await Element).forEach((values, keys) => {
                 // if this language dosent exist in global
@@ -55,7 +48,7 @@ async function mixMap(listMap: Array<Promise<Map<string, { additions: number, de
         })
     });
     await Promise.all(test);
-    console.log({ Global });
+    return Global;
 
 }
 async function getComit(owner: string, repo: string, hash: string) {
@@ -84,7 +77,6 @@ async function getComit(owner: string, repo: string, hash: string) {
     return (nameFile);
 }
 async function listComit(owner: string, repo: string, author?: string) {
-    console.log('list comit of ' + owner + '/' + repo + ' comit by ' + author);
     let listRepo = [];
     let loop: boolean = true;
     let loopIndex: number = 1;
@@ -102,11 +94,44 @@ async function listComit(owner: string, repo: string, author?: string) {
         }
         loopIndex++;
     }
-    const array = listRepo.map(element => {
+    return listRepo.map(element => {
         return getComit(owner, repo, element.sha);
     });
-    mixMap(array);
+}
+async function listAllComitOfUser(userName: string) {
+    console.log('list of repo where ' + userName + ' have contributed');
+    // //list orgs of username
+    const orgs: Array<any> = await getListOrgs(userName);
+    let comits: Array<Promise<Map<string, { additions: number; deletions: number; }>>> = [];
+
+    // in first list all comit created by user in orgs 
+    const sleep = orgs.map(async Org => {
+        // list of repo of orgs
+        const sleep1 = (await getListRepoOfOrg(Org.login)).map(async Repo => {
+            console.log(Org.login + '/' + Repo.name);
+            // add comit of this repo
+            const comitIn = await listComit(Org.login, Repo.name, userName);
+            console.log('in ' + Org.login + '/' + Repo.name + ' : ' + comitIn.length + ' commit');
+            comits = comits.concat(comitIn);
+        });
+        await Promise.all(sleep1);
+    });
+
+    // and list all comit creted in his repo
+
+    const sleep2 = (await getListRepo(userName)).map(async Repo => {
+        // add comit of his repo
+        const comitIn = await listComit(userName, Repo.name, userName);
+        console.log('in ' + userName + '/' + Repo.name + ' : ' + comitIn.length + ' commit');
+        comits = comits.concat(comitIn);
+    });
+    await Promise.all(sleep);
+    await Promise.all(sleep2);
+    console.log('processing of ' + comits.length + ' comit');
+    console.log('please wait');
+    console.log(await mixMap(comits));
 }
 // listComit('Menu-Vaucanson', 'Mobile', 'Wiwok');
-listComit('UnelDev', 'MasterMind');
+// listComit('UnelDev', 'MasterMind');
 // getListRepoOfOrg('Menu-Vaucanson');
+listAllComitOfUser('Wiwok');
